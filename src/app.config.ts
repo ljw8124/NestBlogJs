6005e9c7a8f403cbf0a94dccf145ac28e7d4f099
Joungwoo Lee <ljw8124@gmail.com>
2024-05-22 17:06:16 +0900

    config 생성

---
+++ src/app.config.ts
@@ -0,0 +1,21 @@
+
+require('dotenv').config();
+
+function getEnv(key: string, defValue?: string): string {
+    let reValue = process.env[key] || defValue;
+
+    if(!reValue) {
+        // reValue 없는 경우 에외처리 부분
+        // TODO: 추후 수정 예정
+        reValue = 'ERROR OCCURS';
+    }
+    return reValue;
+}
+
+const SRV_STATE = getEnv('SERVER_STATE');
+
+export const config = {
+    db: {
+        uri: getEnv(`${SRV_STATE}_DB_URI`),
+    },
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,12 @@ import { BlogController } from './blog/blog.controller';
 import { BlogService } from './blog/blog.service';
 import { Blog, BlogSchema } from "./blog/blog.schema";
 import { /*BlogFileRepository,*/ BlogMongoRepository } from "./blog/blog.repository";
-
-require('dotenv').config();
-
+import {config} from "./app.config";
 
 @Module({
   imports: [
       MongooseModule.forRoot(
-          // 해당 값이 null 또는 undefined가 아님을 TypeScript 컴파일러에게 명시적으로 알려주는 것이 '!' 이다.
-          // 하지만 이 값이 실제로 null/undefined 인 경우 런타임에서 에러가 나올 수 있으므로 주의해야한다..
-          process.env.DB_URI!
+          config.db.uri
       ),
       // 몽고디비 스키마 설정
       MongooseModule.forFeature([{name: Blog.name, schema: BlogSchema}]),
