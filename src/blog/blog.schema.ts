2dcc1c85e708af533c650a10cf4fdf8b82f41bb8
Joungwoo Lee <ljw8124@gmail.com>
2024-05-27 16:23:35 +0900

    name 필드 author 로 변경 주석 수정 Schema 옵션 추가(__v 필드제거)

--- src/blog/blog.schema.ts
+++ src/blog/blog.schema.ts
@@ -3,11 +3,11 @@
 import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
 import {Document} from "mongoose";
 
-// 블로크 타입이면서 몽고디비의 도튜먼트로 사용할 수 있는 타입
+// 블로크 타입이면서 몽고디비의 도큐먼트로 사용할 수 있는 타입
 export type BlogDocument = Blog & Document; //& 를 씀으로서 교차타입으로 생성
 
 // 스키마임을 나타냄
-@Schema()
+@Schema({versionKey: false})
 export class Blog {
     // 스키마의 프로퍼티임을 나타냄, @Prop({required: true}) 와 같이 옵션을 추가할 수 있음
     @Prop()
@@ -20,7 +20,7 @@ export class Blog {
     content: string;
 
     @Prop()
-    name: string;
+    author: string;
 
     @Prop()
     createdDt: Date;
