177fe819eb7f008b236226b61d73ce2d3702373c
Joungwoo Lee <ljw8124@gmail.com>
2024-06-04 13:23:01 +0900

    스키마 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { BlogService } from './blog/blog.service';
 import { Blog, BlogSchema } from "./blog/blog.schema";
 import { /*BlogFileRepository,*/ BlogMongoRepository } from "./blog/blog.repository";
 import {config} from "./app.config";
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import {User, UserSchema} from "./user/user.schema";
+import {UserRepository} from "./user/user.repository";
 
 const DB_ADDRESS = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
 
@@ -14,9 +18,17 @@ const DB_ADDRESS = `mongodb://${config.db.host}:${config.db.port}/${config.db.na
           DB_ADDRESS
       ),
       // 몽고디비 스키마 설정
-      MongooseModule.forFeature([{name: Blog.name, schema: BlogSchema}]),
+      MongooseModule.forFeature([
+          {name: Blog.name, schema: BlogSchema},
+          {name: User.name, schema: UserSchema},
+      ]),
   ],
-  controllers: [BlogController],
-  providers: [BlogService, /*BlogFileRepository,*/ BlogMongoRepository],
+  controllers: [
+      BlogController, UserController
+  ],
+  providers: [
+      BlogService, /*BlogFileRepository,*/ BlogMongoRepository,
+      UserService, UserRepository,
+  ],
 })
 export class AppModule {}
