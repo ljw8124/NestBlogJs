53ebf1dcfde6ffa3d6102165cf2e7dc4773d84a9
Joungwoo Lee <ljw8124@gmail.com>
2024-06-05 14:58:00 +0900

    service 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import {UserRepository} from "./user.repository";
+import {UserDto} from "../dto/user.model";
+import {User} from "./user.schema";
 
 @Injectable()
 export class UserService {
     constructor(private userRepository: UserRepository) {}
 
+    async getAllUser() : Promise<UserDto[]> {
+        return await this.userRepository.getAllUser();
+    }
+
+    async createUser(userDto: UserDto) : Promise<void> {
+        return await this.userRepository.createUser(userDto);
+    }
+
+    async getUser(userId: string) : Promise<User> {
+        return await this.userRepository.getUser(userId);
+    }
+
+    async updateUser(userId: string, userDto: UserDto) : Promise<void> {
+        return await this.userRepository.updateUser(userId, userDto);
+    }
+
+    async deleteUser(userDto: UserDto) : Promise<void> {
+        return await this.userRepository.enableUser(userDto);
+    }
 
 }
